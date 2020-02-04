15106c4b56abc988da5fa4fd05a7793dcefd17d5
Adam <adam.kjall@hotmail.com>
2020-02-04 13:10:00 +0100

    added quesestions file with question template

---
+++ src/questions.ts
@@ -0,0 +1,83 @@
+const questions = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "",
+      b: "",
+      c: ""
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "",
+      b: "",
+      c: ""
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "",
+      b: "",
+      c: ""
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "",
+      b: "",
+      c: ""
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "",
+      b: "",
+      c: ""
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "",
+      b: "",
+      c: ""
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "",
+      b: "",
+      c: ""
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "",
+      b: "",
+      c: ""
+    },
+    correctAnswer: "c"
+  }
+];
