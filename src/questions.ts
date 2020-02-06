8d55da54471da8719eaa9c62cf96527f81968c6e
Adam <adam.kjall@hotmail.com>
2020-02-06 14:07:21 +0100

    first commit

--- src/questions.ts
+++ src/questions.ts
@@ -1,6 +1,17 @@
-const questions = [
+type Question = {
+  title: string,
+  answers: {
+    a : string,
+    b: string,
+    c?: string,
+    d?: string
+  },
+  correctAnswer: string
+}
+
+const questions: Question[] = [
   {
-    question: "Hur mycket väger en ishockeypuck?",
+    title: "Hur mycket väger en ishockeypuck?",
     answers: {
       a: "156-170 g",
       b: "171-185 g",
@@ -9,7 +20,7 @@ const questions = [
     correctAnswer: "a"
   },
   {
-    question: "Hur många spelare får maximalt vara på isen från ett lag?",
+    title: "Hur många spelare får maximalt vara på isen från ett lag?",
     answers: {
       a: "5",
       b: "6",
@@ -18,7 +29,7 @@ const questions = [
     correctAnswer: "b"
   },
   {
-    question: "Hur många SM-Guld har Frölunda vunnit?",
+    title: "Hur många SM-Guld har Frölunda vunnit?",
     answers: {
       a: "3",
       b: "5",
@@ -27,7 +38,7 @@ const questions = [
     correctAnswer: "b"
   },
   {
-    question: "Vilken svensk har gjort mest poäng i NHL genom tiderna?",
+    title: "Vilken svensk har gjort mest poäng i NHL genom tiderna?",
     answers: {
       a: "Mats Sundin",
       b: "Börje Salming",
@@ -36,7 +47,7 @@ const questions = [
     correctAnswer: "a"
   },
   {
-    question: "Vad kallas Sveriges herrlandslag?",
+    title: "Vad kallas Sveriges herrlandslag?",
     answers: {
       a: "Kronorna",
       b: "Två Kronor",
@@ -45,7 +56,7 @@ const questions = [
     correctAnswer: "c"
   },
   {
-    question: "Hur många OS-Guld har Tre Kronor vunnit?",
+    title: "Hur många OS-Guld har Tre Kronor vunnit?",
     answers: {
       a: "2",
       b: "4",
@@ -54,7 +65,7 @@ const questions = [
     correctAnswer: "a"
   },
   {
-    question: "Hur många VM-Guld har Tre Kronor vunnit?",
+    title: "Hur många VM-Guld har Tre Kronor vunnit?",
     answers: {
       a: "5",
       b: "8",
@@ -63,7 +74,7 @@ const questions = [
     correctAnswer: "c"
   },
   {
-    question: "Vad heter Sveriges högsta liga för herrar?",
+    title: "Vad heter Sveriges högsta liga för herrar?",
     answers: {
       a: "Allsvenskan",
       b: "SHL",
@@ -72,12 +83,12 @@ const questions = [
     correctAnswer: "b"
   },
   {
-    question: "Har Adam spelat ishockey?",
+    title: "Har Adam spelat ishockey?",
     answers: {
       a: "Nej!",
       b: "Nej, han kan inte åka skridskor!",
       c: "Nej, han hatar hockey!"
     },
-    correctAnswer: "a"
+    correctAnswer: "b"
   }
 ];
--- src/script.ts
+++ src/script.ts
@@ -1,57 +1,95 @@
-const startButton = document.querySelector(".start") as HTMLButtonElement;
-const questionTitle = document.querySelector("h2") as HTMLElement;
-const radioButtonLabels = document.querySelectorAll(".answer-container > label") as NodeList;
-const answerContainer = document.querySelector(".answer-container");
-const answerForm = document.querySelector(".answer-form") as HTMLFormElement;
-const submitButton = document.querySelector(".submit");
-
-startButton.addEventListener("click", startQuiz);
-
-submitButton.addEventListener("click", submitAnswer);
+const startButton = document.querySelector(
+  ".start-button"
+) as HTMLButtonElement;
+const startContainer = document.querySelector(
+  ".start-container"
+) as HTMLElement;
+const questionContainer = document.querySelector(
+  ".question-container"
+) as HTMLElement;
+const questionTitle = document.querySelector(".question") as HTMLElement;
+const answerButtons = document.querySelector(".answer-buttons") as HTMLElement;
+const nextButton = document.querySelector(".next-button") as HTMLButtonElement;
 
 let questionNumber = 0;
 
-
-function startQuiz() {
-  showQuiz();
-  nextQuestion();
-  
-}
-
-function showQuiz() {
-  startButton.style.display = "none";
-  questionTitle.style.display = "block";
-  answerForm.style.display = "flex";
-}
-
-function submitAnswer() {
-  nextQuestion();
-}
-
-function showResult() {
-}
-
-function nextQuestion() {
-  const {question, answers, correctAnswer} = questions[questionNumber];
-  
-  questionTitle.innerText = question;
-  answerContainer.innerHTML = "";
-
-  for(let letter in answers) {
-    const inputMarkUp = `<label>
-    <input type="radio" name="question${questionNumber}" value="${letter}">
-    ${answers[letter]}
-    </label>`
-   answerContainer.innerHTML += inputMarkUp;
-  }
-
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
   questionNumber++;
+  if (questionNumber < questions.length) {
+    setNextQuestion();
+
+  } else { // slut
+    // dölj nästa knappen
+    nextButton.classList.add("hide");
+
+    // skapa en spela igen knapp
+    const playAgain = document.createElement("button") as HTMLButtonElement;
+    playAgain.innerText = "Play again";
+    playAgain.addEventListener("click", () => {
+      startGame();
+      // ta bort spela igen knappen
+      questionContainer.removeChild(playAgain)
+    });
+
+    // lägg till spepla igen knappen
+    questionContainer.appendChild(playAgain)
+
+  }
+})
+
+function startGame() {
+  startContainer.classList.add("hide");
+  questionContainer.classList.remove("hide");
+
+  questionNumber = 0;
+  setNextQuestion();
 }
 
-function showEndGameResult () {
-  
+function setNextQuestion() {
+  resetState();
+  showQuestion(questions[questionNumber]);
 }
 
-function playAgain() {
-  
+function showQuestion(question: Question) {
+  questionTitle.innerText = question.title;
+
+  const letters = Object.keys(question.answers);
+
+  letters.forEach(letter => {
+    const button = document.createElement("button");
+    const answer = question.answers[letter];
+    button.innerText = answer;
+
+    if (letter === question.correctAnswer) {
+      button.dataset.correct = letter;
+    }
+
+    button.addEventListener("click", selectAnswer);
+
+    answerButtons.appendChild(button);
+  });
+}
+
+function selectAnswer(e: Event) {
+  // visa next knappen
+  nextButton.classList.remove("hide");
+  // knappen som blev klicka på
+  const selectedButton = e.target as HTMLButtonElement;
+  // hämta svaret från html elementets dataset
+  //const correct = selectedButton.dataset.correct;
+
+  // ändra knapparnas färger
+  Array.from(answerButtons.children).forEach((button: HTMLButtonElement) => {
+    if (button.dataset.correct) {
+      button.classList.add("correct")
+    } else {
+      button.classList.add("wrong")
+    }
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  answerButtons.innerHTML = "";
 }
